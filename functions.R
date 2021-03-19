8bf83d434192dc37ba21ca074efdf83cf002cf37
thomascrines <thomascrines@hotmail.com>
2021-03-19 14:27:49 +0000

    Update functions.R
    Add posixct to get sql data type function

--- functions.R
+++ functions.R
@@ -182,6 +182,7 @@ adm_i_r_to_sql_data_type <- function(r_data_type) {
                           "character" = "varchar(255)",
                           "factor" = "varchar(255)",
                           "POSIXct" = "datetime2(3)",
+                          "POSIXlt" = "datetime2(3)",
                           "integer" = "int")
   
   sql_data_type
