6bb961f8e27ca966af5744630f6cc832f2e9c25d
thomascrines <thomascrines@hotmail.com>
2021-04-29 16:55:45 +0100

    Update script.R

--- script.R
+++ script.R
@@ -1,42 +1,74 @@
-# Populate with values provided by ADM team
+# Load functions
+source("functions.R")
+
+# Assign variables (provided by ADM team)
 server <- ""
 database <- ""
 schema <- ""
 
-# Name you want table to have
-table_name <- ""
-
-# List tables currently in database
-adm_list_tables(database, server)
-
-# View metadata of table in database
-metadata <- adm_metadata_columns(database, server, schema, table_name)
-
-# Import entire table from database
-table_from_db <- adm_import_table(database, server, schema, table_name)
-
-# Import selected columns from table in database
-columns <- c('ColumnName1', 'ColumnName2') # Populate with existing column names
-table_from_db <- adm_import_table(database, server, schema, table_name, columns)
-
-# Import selected columns from table in database between selected rows
-columns <- c('ColumnName1', 'ColumnName2') # Populate with existing column names
-start_row <- 1 # If NULL will start from beginning of table
-end_row <- 3 # If NULL will end at end of table
-table_from_db <- adm_import_table(database, server, schema, table_name, columns, start_row, end_row)
-
-# Example loop to bring back selected columns from selected tables
-columns <- c('ColumnName1', 'ColumnName2') # Populate with existing column names
-tables <- c('TableName1', 'TableName2') # Populate with existing table names
-
-for (table in tables) {
-  
-  table_name <- table
-  table_from_db <- adm_import_table(database, server, schema, table_name, columns)
-  assign(table_name, table_from_db)
-}
+# Assign dummy data for examples, can assign desired values as needed
+table_name <- "ExampleUpload" # The name the table will have in the database 
+dataframe <- iris             # The dataframe to upload
 
 # Upload dataframe to database
-table_name <- "ExampleUpload"
-dataframe <- iris
-adm_upload_dataframe(database, server, schema, table_name, dataframe)
+adm_write_table_to_db(database = database, 
+                      server = server, 
+                      schema = schema, 
+                      table_name = table_name, 
+                      dataframe = dataframe)
+
+# List tables currently in database
+adm_db_tables(database = database, 
+              server = server)
+
+# View metadata of table in database
+table_metadata <- adm_db_table_metadata(database = database, 
+                                        server = server, 
+                                        schema = schema, 
+                                        table_name = table_name)
+
+# Import entire table from database
+table_from_db <- adm_read_table_from_db(database = database, 
+                                        server = server, 
+                                        schema = schema, 
+                                        table_name = table_name)
+
+# Import selected columns from table in database
+columns <- c('Sepal.Length', 'Sepal.Width') # Populate with existing column names
+
+table_from_db <- adm_read_table_from_db(database = database, 
+                                        server = server, 
+                                        schema = schema, 
+                                        table_name = table_name, 
+                                        columns = columns)
+
+# Import selected rows from table in database
+columns <- NULL
+start_row <- 1 # If NULL will start from beginning of table
+end_row <- 3 # If NULL will end at end of table
+
+table_from_db <- adm_read_table_from_db(database = database, 
+                                        server = server, 
+                                        schema = schema, 
+                                        table_name = table_name, 
+                                        columns = columns, 
+                                        start_row = start_row, 
+                                        end_row = end_row)
+
+# Import selected columns from table in database between selected rows
+columns <- c('Sepal.Length', 'Sepal.Width') # Populate with existing column names
+start_row <- 1 # If NULL will start from beginning of table
+end_row <- 3 # If NULL will end at end of table
+table_from_db <- adm_read_table_from_db(database = database, 
+                                        server = server, 
+                                        schema = schema, 
+                                        table_name = table_name, 
+                                        columns = columns, 
+                                        start_row = start_row, 
+                                        end_row = end_row)
+
+# Delete table completely from database
+adm_drop_table_from_db(database = database, 
+                       server = server, 
+                       schema = schema, 
+                       table_name = table_name)
